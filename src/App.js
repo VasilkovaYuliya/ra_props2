2f3c3a994bdb4bdd0e3579c3c5f566f74d77a034
VasilkovaYuliya <2st.vasilkou@gmail.com>
2023-05-10 16:30:21 +0300

    Вторая задача изменена

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Listing from './Listing';
 import data from './data.json';
 
-function App() {
+const App = () => {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    setItems(data.map(item => JSON.parse(item)));
+    setItems(data);
   }, []);
 
-  return <Listing items={items} />;
-}
+  return (
+    <div>
+      <h1>Список предложений</h1>
+      <Listing items={items} />
+    </div>
+  );
+};
 
 export default App;
--- src/Listing.js
+++ src/Listing.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './styles.css';
 
-const Listing = ({ items = [] }) => {
+const Listing = ({ items }) => {
+  if (!items || !Array.isArray(items)) {
+    return null;
+  }
+
   return (
     <div className="item-list">
       {items.map(item => (
         <div className="item" key={item.listing_id}>
           <div className="item-image">
             <a href={item.url}>
-              <img src={item.MainImage.url_570xN} alt={item.title} />
+              <img src={item.MainImage && item.MainImage.url_570xN} alt={item.title} />
             </a>
           </div>
           <div className="item-details">
-            <p className="item-title">{item.title.length > 50 ? item.title.slice(0, 50) + '...' : item.title}</p>
+            <p className="item-title">{item.title && item.title.length > 50 ? item.title.slice(0, 50) + '...' : item.title}</p>
             <p className="item-price">{item.currency_code === 'USD' ? '$' : item.currency_code === 'EUR' ? '€' : item.currency_code + ' '} {item.price}</p>
             <p className={`item-quantity ${item.quantity <= 10 ? 'level-low' : item.quantity <= 20 ? 'level-medium' : 'level-high'}`}>{item.quantity} left</p>
           </div>
